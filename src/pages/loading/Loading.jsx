ccae0108a9649818dbc64f38d48be52563a763a6
yousung <doyousung@naver.com>
2025-08-13 02:45:27 +0900

    이미지파일 외 수정 완료

--- src/pages/loading/Loading.jsx
+++ src/pages/loading/Loading.jsx
@@ -4,9 +4,9 @@ import './Loading.css';
 const Loading = () => {
   return (
     <div className="loading-container">
-      <p style={{ fontWeight: 'bold', fontSize: '50px', marginTop: '166px' }}>미션 생성중</p>
+      <p style={{ fontWeight: '800', fontSize: '40px', marginTop: '166px' }}>미션 생성중</p>
       <img src={loadingIcon} alt="로딩 아이콘" className="loading-icon"/>
-      <p style={{ fontWeight: 'bold', fontSize: '15px', marginTop: '49px' }}>핸썹이가 오늘의 미션을 열심히 준비 중이에요!</p>
+      <p style={{ fontWeight: '600', fontSize: '16px', marginTop: '49px' }}>핸썹이가 오늘의 미션을 열심히 준비 중이에요!</p>
     </div>
   );
 };
