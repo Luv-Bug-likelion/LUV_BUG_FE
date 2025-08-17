66c2c8b32d952b205756e6911e24f591bf32388d
yousung <doyousung@naver.com>
2025-08-17 13:28:38 +0900

    연결오류 수정(무조건 dev 항상 pull)

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    host: true,
+    https: {
+      key: "./localhost-key.pem", // 3단계에서 생성된 키 파일
+      cert: "./localhost.pem", // 3단계에서 생성된 인증서 파일
+    },
+  },
 });
