547f2dbff009b428eb0000561dfa72b6869bf7f4
Goeunwoo <avouchre24@gmail.com>
2025-08-18 20:06:31 +0900

    feat: Info 페이지 API 연동 수정

--- vite.config.js
+++ vite.config.js
@@ -10,13 +10,5 @@ export default defineConfig({
       key: "./localhost-key.pem",
       cert: "./localhost.pem",
     },
-    proxy: {
-      "/api": {
-        target: "https://18.218.186.60:8080", // 백엔드 주소 (http면 http로!)
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""), // /api/home -> /home
-      },
-    },
   },
 });
