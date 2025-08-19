59cbe9d420931fc24f527ddda38e6c5774fe9c72
yousung <doyousung@naver.com>
2025-08-19 10:47:52 +0900

    미션설명모달수정완료

--- src/components/Explain.jsx
+++ src/components/Explain.jsx
@@ -10,10 +10,10 @@ const Explain = ({ isOpen, onClose, data }) => {
     <div className="modal-backdrop" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         {/* API로 받은 데이터가 있다면 그 데이터를, 없다면 기본 메시지를 표시합니다. */}
-        <p>미션 생성완료!</p>
-        <img src={handsup} alt='핸썹이' style={{}} />
-        <p>{data?.explain ?? '미션 설명을 불러올 수 없습니다.'}</p>
-        <button onClick={onClose}>미션하러 가기</button>
+        <p style={{fontSize:'20px', fontWeight:'800', color:'#2d2d2d', marginTop:'25px'}}>미션 생성완료!</p>
+        <img src={handsup} alt='핸썹이' style={{width:'118px', height:'160px', marginTop:'20px'}} />
+        <p style={{fontSize:'12px', fontWeight:'600', lineHeight:'15px', marginTop:'20px'}}>{data?.explain ?? '미션 설명을 불러올 수 없습니다.'}</p>
+        <button className='explain-modal-btn' onClick={onClose}>미션하러 가기</button>
       </div>
     </div>
   );
